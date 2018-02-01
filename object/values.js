f108a08fcb1295860818848dd890d50829d3584b
KSH-code <tjdgnsqn133@gmail.com>
2018-02-01 11:06:25 +0900

    test object values performance

--- object/values.js
+++ object/values.js
@@ -3,13 +3,21 @@ const b = {}
 for (let i = 0; i < 1000; i++) {
   b[`${i}${i}`] = true
 }
-console.time('valuesa')
+console.time('use values for empty object')
 for (let i = 0; i < 10000; i++) {
   Object.values(a)
 }
-console.timeEnd('valuesa')
-console.time('valuesb')
+console.timeEnd('use values for empty object')
+console.time('use values')
 for (let i = 0; i < 10000; i++) {
-  Object.values(b)
+  Object.values(b).join('')
 }
-console.timeEnd('valuesb')
+console.timeEnd('use values')
+console.time('not use values')
+for (let i = 0; i < 10000; i++) {
+  let count = ''
+  for (let key in b) {
+    count += b[key]
+  }
+}
+console.timeEnd('not use values')
