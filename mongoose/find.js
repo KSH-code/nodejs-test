5a02959b50777b743d362d4b4fa084c7426c133c
KSH-code <tjdgnsqn133@gmail.com>
2018-02-06 17:55:52 +0900

    조금 더 많은 방법으로 find

--- mongoose/find.js
+++ mongoose/find.js
@@ -1,9 +1,11 @@
-// find name1 만 인덱스 적용: 15590.580ms
-// find 인덱스: 3702.730ms
-// find 인덱스 아닌거: 40035.381ms
-// findOne name1 만 인덱스 적용: 8391.551ms
-// findOne 인덱스: 2794.259ms
-// findOne 인덱스 아닌거: 40759.905ms
+// find name1 인덱스 name2 기본: 14364.390ms
+// find name1 인덱스 find name3 인덱스: 14756.065ms
+// find name1 인덱스: 3420.016ms
+// find 인덱스 아닌거: 46169.341ms
+// findOne name1 만 인덱스 적용: 14874.725ms
+// findOne name1 찾고 findOne name3: 14839.920ms
+// findOne 인덱스: 2491.297ms
+// findOne 인덱스 아닌거: 49180.157ms
 const mongoose = require('mongoose')
 mongoose.connect(`mongodb://localhost/test`, err => {
   if (err) {
@@ -15,6 +17,9 @@ mongoose.connect(`mongodb://localhost/test`, err => {
     },
     name2: {
       type: String
+    },
+    name3: {
+      type: String
     }
   }, {
       timestamps: {
@@ -24,6 +29,7 @@ mongoose.connect(`mongodb://localhost/test`, err => {
     }
   )
   scopeSchema1.index({ name1: 1 })
+  scopeSchema1.index({ name1: 3 })
   const model1 = mongoose.model('asdhasjdhahzxcn', scopeSchema1)
     ; (async function () {
       try {
@@ -32,7 +38,8 @@ mongoose.connect(`mongodb://localhost/test`, err => {
           for (let j = 0; j < 1000; j++) {
             await new model1({
               name1: `${i}`,
-              name2: `${j}ag`
+              name2: `${j}ag`,
+              name2: `${j}`
             }).save()
           }
         }
@@ -42,26 +49,39 @@ mongoose.connect(`mongodb://localhost/test`, err => {
             await model1.find({ name1: `${i}`, name2: `${j}ag` })
           }
         }
-        console.time('find name1 만 인덱스 적용')
+        console.time('find name1 인덱스 name2 기본')
         for (let i = 0; i < 10; i++) {
           for (let j = 0; j < 1000; j++) {
             await model1.find({ name1: `${i}`, name2: `${j}ag` })
           }
         }
-        console.timeEnd('find name1 만 인덱스 적용')
+        console.timeEnd('find name1 인덱스 name2 기본')
+
+        for (let i = 0; i < 10; i++) {
+          for (let j = 0; j < 1000; j++) {
+            await model1.find({ name1: `${i}` }).find({ name3: `${j}` })
+          }
+        }
+        console.time('find name1 인덱스 find name3 인덱스')
+        for (let i = 0; i < 10; i++) {
+          for (let j = 0; j < 1000; j++) {
+            await model1.find({ name1: `${i}` }).find({ name3: `${j}` })
+          }
+        }
+        console.timeEnd('find name1 인덱스 find name3 인덱스')
 
         for (let j = 0; j < 10; j++) {
           for (let i = 0; i < 1000; i++) {
             await model1.find({ name1: `${i}` })
           }
         }
-        console.time('find 인덱스')
+        console.time('find name1 인덱스')
         for (let j = 0; j < 10; j++) {
           for (let i = 0; i < 1000; i++) {
             await model1.find({ name1: `${i}` })
           }
         }
-        console.timeEnd('find 인덱스')
+        console.timeEnd('find name1 인덱스')
 
         for (let i = 0; i < 10; i++) {
           for (let j = 0; j < 1000; j++) {
@@ -88,6 +108,19 @@ mongoose.connect(`mongodb://localhost/test`, err => {
           }
         }
         console.timeEnd('findOne name1 만 인덱스 적용')
+
+        for (let i = 0; i < 10; i++) {
+          for (let j = 0; j < 1000; j++) {
+            await model1.findOne({ name1: `${i}` }).findOne({ name3: `${j}` })
+          }
+        }
+        console.time('findOne name1 찾고 findOne name3')
+        for (let i = 0; i < 10; i++) {
+          for (let j = 0; j < 1000; j++) {
+            await model1.findOne({ name1: `${i}` }).findOne({ name3: `${j}` })
+          }
+        }
+        console.timeEnd('findOne name1 찾고 findOne name3')
 
         for (let j = 0; j < 10; j++) {
           for (let i = 0; i < 1000; i++) {
