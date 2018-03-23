e98598664371776f5cd95df9f54800c7a911c382
ksh-code <tjdgnsqn133@gmail.com>
2018-03-23 16:51:07 +0900

    create 추가

--- mongoose/wrapping-insertmany.js
+++ mongoose/wrapping-insertmany.js
@@ -1,17 +1,23 @@
-// insertMany: 705.555ms
-// { name1: 'hello', _id: 5ab4aab38a051a4089d55729, __v: 0 }
-// insertMany: 456.353ms
-// { name1: 'hello', _id: 5ab4aab38a051a4089d55729, __v: 0 }
-// save: 28790.550ms
-// save: 31028.968ms
-// { name1: 'hello', _id: 5ab4aab48a051a4089d5f369, __v: 0 }
-// insertMany: 699.071ms
-// { name1: 'hello', _id: 5ab4ab1536a2cf41b9e90c70, __v: 0 }
-// insertMany: 516.262ms
-// { name1: 'hello', _id: 5ab4ab1536a2cf41b9e90c70, __v: 0 }
-// save: 32310.738ms
-// save: 35001.288ms
-// { name1: 'hello', _id: 5ab4ab1636a2cf41b9e9a8b0, __v: 0 }
+// insertMany: 172.058ms
+// { name1: 'hello', _id: 5ab4b1c247a46b5691778250, __v: 0 }
+// insertMany: 94.320ms
+// { name1: 'hello', _id: 5ab4b1c247a46b5691778250, __v: 0 }
+// save: 515.782ms
+// save: 443.502ms
+// { name1: 'hello', _id: 5ab4b1c247a46b56917791f0, __v: 0 }
+// create: 442.172ms
+// create: 393.982ms
+// { name1: 'hello', _id: 5ab4b1c347a46b56917799c4, __v: 0 }
+// insertMany: 163.793ms
+// { name1: 'hello', _id: 5ab4b1c62e504256a1b9df75, __v: 0 }
+// insertMany: 87.504ms
+// { name1: 'hello', _id: 5ab4b1c62e504256a1b9df75, __v: 0 }
+// save: 513.205ms
+// save: 456.415ms
+// { name1: 'hello', _id: 5ab4b1c62e504256a1b9ef15, __v: 0 }
+// create: 417.969ms
+// create: 431.644ms
+// { name1: 'hello', _id: 5ab4b1c72e504256a1b9f6e6, __v: 0 }
 const mongoose = require('mongoose')
 mongoose.connect(`mongodb://localhost/test`, err => {
   if (err) {
@@ -29,14 +35,14 @@ mongoose.connect(`mongodb://localhost/test`, err => {
         await mongoose.connection.db.dropDatabase()
         console.time('insertMany')
         let arr = []
-        for (let i = 0; i < 1e4; i++) arr.push(new model1())
+        for (let i = 0; i < 1e3; i++) arr.push(new model1())
         await model1.insertMany(arr)
         console.timeEnd('insertMany')
         console.log(await model1.findOne())
 
         console.time('insertMany')
         arr = []
-        for (let i = 0; i < 1e4; i++) arr.push(new model1())
+        for (let i = 0; i < 1e3; i++) arr.push(new model1())
         await model1.insertMany(arr)
         console.timeEnd('insertMany')
         console.log(await model1.findOne())
@@ -44,15 +50,29 @@ mongoose.connect(`mongodb://localhost/test`, err => {
         await mongoose.connection.db.dropDatabase()
         console.time('save')
         arr = []
-        for (let i = 0; i < 1e4; i++) arr.push(new model1().save())
+        for (let i = 0; i < 1e3; i++) arr.push(new model1().save())
         await Promise.all(arr)
         console.timeEnd('save')
 
         console.time('save')
         arr = []
-        for (let i = 0; i < 1e4; i++) arr.push(new model1().save())
+        for (let i = 0; i < 1e3; i++) arr.push(new model1().save())
         await Promise.all(arr)
         console.timeEnd('save')
+        console.log(await model1.findOne())
+
+        await mongoose.connection.db.dropDatabase()
+        console.time('create')
+        arr = []
+        for (let i = 0; i < 1e3; i++) arr.push(new model1())
+        await model1.create(arr)
+        console.timeEnd('create')
+
+        console.time('create')
+        arr = []
+        for (let i = 0; i < 1e3; i++) arr.push(new model1())
+        await model1.create(arr)
+        console.timeEnd('create')
         console.log(await model1.findOne())
         await mongoose.disconnect()
     })()
